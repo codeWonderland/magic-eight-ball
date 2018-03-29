8b3301946bb5466122cd9f05ea22d2de0ab854e3
Alice Easter <alice.easter@mymail.champlain.edu>
2018-03-29 13:36:08 -0400

    Well, we have some shit now

--- client.py
+++ client.py
@@ -1,2 +1,34 @@
+from _socket import socket
+
+import sys
+
+"""
+>>> data = 'abcdefg'
+>>> [delim for delim in ('a', 'e') if delim in data]
+['a', 'e']
+"""
+
+class EightBallClient:
+    def __init__(self, hostname, port):
+        # getaddrinfo functionality replicated from https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter04/www_ping.py
+        try:
+            info_list = socket.getaddrinfo(hostname, port, flags=socket.AI_ADDRCONFIG | socket.AI_V4MAPPED)
+        except socket.gaierror as e:
+            print('Failure in getaddrinfo: ', e.args[1])
+            sys.exit(1)
+
+        info = info_list[0]
+        socket_args = info[0:3]
+        address = info[4]
+        self.sock = socket.socket(*socket_args)
+        try:
+            self.sock.connect(address)
+        except socket.error as e:
+            print('Network failure: ', e.args[1])
+            sys.exit(1)
+        else:
+            print('Successfully connected to host ', info[3], ' is listening')
+            # Client code here
+
 if __name__ == "__main__":
     print("hello world")
